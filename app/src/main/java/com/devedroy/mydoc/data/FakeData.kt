9041c6438c2ba4696a2a37241ce3802df0663c9c
Roy <devedroy.dr@gmail.com>
2023-03-27 19:26:28 +0530

    added fake data

--- app/src/main/java/com/devedroy/mydoc/data/FakeData.kt
+++ app/src/main/java/com/devedroy/mydoc/data/FakeData.kt
@@ -5,9 +5,15 @@ import com.devedroy.mydoc.data.local.Hospital
 import com.devedroy.mydoc.data.local.Surgery
 import com.devedroy.mydoc.data.local.Test
 
-fun fillHospitalData(): List<Hospital> = listOf(
-    Hospital("Nizam's", "Hyderabad", 4.9f)
-)
+fun fillHospitalData(): List<Hospital> {
+    var hospitals  = mutableListOf(Hospital("Nizam's", "Hyderabad", 4.9f))
+    for (i in 0..100) {
+        hospitals.add(Hospital("Nizam's", "Hyderabad", 4.9f))
+    }
+    return hospitals
+}
+
+
 
 fun fillSurgeriesData(): List<Surgery> = listOf(
     Surgery("Heart Operation", 300000f)
